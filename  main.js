9f8a592bf16bd7cb66fb212f382de7704d89e157
monster1935 <946815480@qq.com>
2018-08-21 12:34:30 +0800

    in user1/dev feature

---  main.js
+++  main.js
@@ -6,9 +6,16 @@ export function testUser1() {
     console.log('test user1');
     console.log('add a line');
     console.log('add second line');
+    console.log('fourth modified');
+
 }
 
 export function study() {
     console.log('fun study');
     console.log('fun line 2');
+
+}
+
+export function test2() {
+    console.log('in test2 function')
 }
